e8b773ab1a616ec40f2ba6f980c42c5c7e742211
Aro-Kira <minenetwork28@gmail.com>
2025-11-14 01:02:41 +0800

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Glassify-CI/';
+// $config['base_url'] = 'http://localhost/Glassify-CI/';
+$config['base_url'] = 'https://aro-kira.github.io/Glassify-CI/';
 
 /*
 |--------------------------------------------------------------------------
